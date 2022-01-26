68d0a83a5c57bcf71425dc3804cbea7e2c722bf4
Vanessa <vdbui25@students.bmchs.org>
2022-01-26 10:01:48 -0600

    Do now 2.02

---
+++ Do_Now_2.02.txt
@@ -0,0 +1,41 @@
+############################
+Do Now 2.02
+############################
+----------------------------
+Part 1
+----------------------------
+Open up the console. Type each line of the following code into the interactive editor:
+ >>> 5 < 3
+ >>> 5 > 3
+ >>> type(5 < 3)
+ >>> type(5 > 3)
+ >>> my_favorite_animal = "cats"
+ >>> user_favorite_animal = input("What is your favorite animal? ")
+ >>> my_favorite_animal == user_favorite_animal
+In your notebook, answer the following
+What does 5 < 3 evaluate to?  
+What is the type of 5 < 3? What does that stand for (should be familiar from Snap!)?
+What is the difference between == and = ?
+What data type do you think my_favorite_animal == user_favorite_animal is?
+----------------------------
+Part 2
+----------------------------
+Open up the console. Type the following code into the interactive editor.
+ >>> months_with_driving_permit = 6
+ >>> age = 16
+ >>> can_get_license = months_with_driving_permit >= 6 and age >= 16
+ >>> print(can_get_license)
+In your Notebook, add this to your notes in your notebook
+What does 'and' do here? What type do you think can_get_license is?
+Update the code to fit the new driving law: If you are over the age of 18 you don't need to have a permit
+----------------------------
+Part 3
+----------------------------
+Open up the console. Type the following code into the interactive editor.
+>>> animal = 'mouse'
+>>> animal == 'cat' or 'dog'
+>>> animal = 'mouse'
+>>> animal == 'cat' or animal == 'dog'
+In your notebook, continue answers to the following
+What does = and == do here?
+What is the difference between the two 'or' statements?
---
+++ Lab 2.02.py
@@ -0,0 +1,53 @@
+'''
+##################################
+Lab 2.02 Booleans & Expressions
+##################################
+In your Notebook
+Predict if each of the following examples will produce a True or False output. Check your answers in interactive mode.
+                 
+Example 1
+    >>> a = 100
+    >>> b = "science"
+    >>> a > 75 and b == "science"       Prediction:                     Actual:
+Example 2
+    >>> a = 100
+    >>> b = "science"
+    >>> a > 75 and b != "science"       Prediction:                     Actual:
+Example 3
+    >>> a = 100
+    >>> b = "science"
+    >>> a > 75 or b != "science"        Prediction:                     Actual:
+Example 4
+    >>> a = 100
+    >>> b = "science"
+    >>> c = True
+    >>> not c and a > 75 and b == "science"     Prediction:                     Actual:
+
+In your Console
+
+Complete the following coding challenge
+Create a "Can I be President?" program, which determines if the user meets the minimum requirements for becoming the President of the United States. 
+Have the user input the information needed.
+
+The minimum requirements to be president of the United States are:
+
+1. Older than 35
+
+2. Resident of US for 14 Years
+
+3. Natural born citizen
+
+Print True if the person could be president and False if they can't be president.
+
+Create a "I can't be President?" program. Print True if the user cannot be President and False if they can be President.
+
+Create a "Can I ride the roller coaster?" program. A roller coaster has the rule that a rider has to be over the height of 50 inches. Because of a legal loophole, if you are over the age of 18 you can ride regardless of your height. If you are allowed to ride, the coaster costs 4 quarters (although the operator accepts tips so more money is appreciated).
+
+Also, the theme park sells frequent rider passes: with a frequent rider pass the roller coaster costs only 2 quarters. Ask the user how tall they are in inches, their age, how many quarters they have, and if they have a frequent rider pass. Print True if the person can ride and False if they can't.
+
+
+Are the following expressions equivalent? Research DeMorgan's Laws and write why you think they are the same or why they are not the same
+not(x or y) == not x and not y
+
+not(x and y) == not x or not y
+'''
