b821aeb80616fc7dbdd6933b6314272dfce46fa0
vansssab <vdbui25@students.bmchs.org>
2022-02-09 21:07:28 -0600

    Lab 2.06

--- Lab_2.06.py
+++ Lab_2.06.py
@@ -61,7 +61,6 @@ while a != "y" and a != "n" :
 '''
 #tic tac toe
 spots = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-square = ''
 turns = 0
 print(f"{spots[0]} | {spots[1]} | {spots[2]} \n"
     "--------- \n"
@@ -69,31 +68,77 @@ print(f"{spots[0]} | {spots[1]} | {spots[2]} \n"
     "--------- \n"
     f"{spots[6]} | {spots[7]} | {spots[8]}")
 print("Let's play Tic-Tac-Toe!")
+print("The first player will be X and the second player will be O.")
 while turns < 9:
-    print("The first player will be X and the second player will be O.")
-    while spots[square - 1] == 'X' or spots[square - 1] == 'O':  
-        square = int(input("Which square would you like to mark? "))
-    if turns == 1:
-        spots[square - 1] = 'X'
-    elif turns == 2:
-        spots[square - 1] = 'O'
-    elif turns == 3:
-        spots[square - 1] = 'X'
-    elif turns == 4:
-        spots[square - 1] = 'O'
-    elif turns == 5:
-        spots[square - 1] = 'X'
-    elif turns == 6:
-        spots[square - 1] = 'O'
-    elif turns == 7:
-        spots[square -1 ] = 'X'
-    elif turns == 8:
-        spots[square - 1] = 'O'
-    elif turns == 9:
-        spots[square -1] = 'X'
+    print("Player 1")  
+    mark = input("Which square would you like to mark? ")
+    if mark == '1':
+        spots[0] = 'X'
+        turns = turns + 1
+    elif mark == '2':
+        spots[1] = 'X'
+        turns = turns + 1
+    elif mark == '3':
+        spots[2] = 'X'
+        turns = turns + 1
+    elif mark == '4':
+        spots[3] = 'X'
+        turns = turns + 1
+    elif mark == '5':
+        spots[4] = 'X'
+        turns = turns + 1
+    elif mark == '6':
+        spots[5] = 'X'
+        turns = turns + 1
+    elif mark == '7':
+        spots[6] = 'X'
+        turns = turns + 1
+    elif mark == '8':
+        spots[7] = 'X'
+        turns = turns + 1
+    elif mark == '9':
+        spots[8] = 'X'
+        turns = turns + 1
     else:
         print('Sorry, that is not a spot on the board.')
-    turns[0] = turns[0] + 1
+        turns = turns + 1
+    print(f"{spots[0]} | {spots[1]} | {spots[2]} \n"
+    "--------- \n"
+    f"{spots[3]} | {spots[4]} | {spots[5]} \n"
+    "--------- \n"
+    f"{spots[6]} | {spots[7]} | {spots[8]}")
+    print("Player 2")
+    mark = input("Which square would you like to mark? ")
+    if mark == '1':
+        spots[0] = 'O'
+        turns = turns + 1
+    elif mark == '2':
+        spots[1] = 'O'
+        turns = turns + 1
+    elif mark == '3':
+        spots[2] = 'O'
+        turns = turns + 1
+    elif mark == '4':
+        spots[3] = 'O'
+        turns = turns + 1
+    elif mark == '5':
+        spots[4] = 'O'
+        turns = turns + 1
+    elif mark == '6':
+        spots[5] = 'O'
+        turns = turns + 1
+    elif mark == '7':
+        spots[6] = 'O'
+        turns = turns + 1
+    elif mark == '8':
+        spots[7] = 'O'
+        turns = turns + 1
+    elif mark == '9':
+        spots[8] = 'O'
+        turns = turns + 1
+    else:
+        print('Sorry, that is not a spot on the board.')
+        turns = turns + 1
     print(f"{spots[0]} | {spots[1]} | {spots[2]} \n"
     "--------- \n"
     f"{spots[3]} | {spots[4]} | {spots[5]} \n"
