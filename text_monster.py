48ebc375dad9e39320991c8cb467f11376ff027d
Vanessa <vdbui25@students.bmchs.org>
2022-02-16 10:03:18 -0600

    user input challenge

--- text_monster.py
+++ text_monster.py
@@ -3,17 +3,19 @@ start = input("Type start to start this adventure. ")
 if start == 'start':
     print("Here are the instructions: \n"
     "There are 5 rooms on 3 floors . \n"
-    "Each room will wither have a sword, a monster, magic stones, up-stairs, down-stairs, or nothing. \n"
+    "Each room will either have a sword, a monster, magic stones, up-stairs, down-stairs, or nothing. \n"
     "You will be going through 3 floors and you can either go down or up the floors. \n"
-    "Your goal is to find and defeat the boss monster, but before you do that, you need a sword and magic stones to do this \n"
+    "You can go left, right, up, down, grab, or fight. \n"
+    "You can also access you inventory whenever you want.\n"
+    "Your goal is to find and defeat the boss monster, but before you do that, you need a sword and magic stones. \n"
     "You will use only the sword to defeat the monster in rendom rooms, but it will dissapear after you use it. \n"
     "Good Luck and Have Fun!")
 
 #Map of dungeon
 dungeon = [
-    ['sword', 'stairs_down', 'empty', 'empty', 'boss_monster'], 
-    ['magic_stones', 'monster', 'stairs_up', 'empty', 'stairs_down'], 
-    ['empty', 'sword', 'stairs_up', 'monster', 'empty']
+    ['sword', 'monster', 'stairs_down', 'boss_monster', 'prize'], 
+    ['magic_stones', 'monster', 'stairs_up', 'stairs_down', 'sword'], 
+    ['empty', 'sword', 'stairs_up', 'monster', 'sword']
     ]
 
 #player_info
@@ -43,25 +45,93 @@ while True:
         print("There are stairs that go down in the room.")
     elif location == 'boss_monster':
         print("You found the boss monster.")
+    else:
+        ("You made it to the prize!")
     
     #player choices
-    player_choices = input("What would you like to do? (left, right, up, down, grab, fight) ")
+    player_choices = input("What would you like to do? (left, right, up, down, grab, fight, inventory.) ")
     print(player_choices)
     if player_choices == 'right':
         current_room += 1
         if current_room == 5:
             print("You can't go any further in that direction.")
-            current_room -= 1
+            current_room = 4
+        elif location == 'monster' or location == 'boss_monster':
+            print("You cannot go past a monster. Therefore, you have been eaten and died.")
+            print("Game Over")
+            break
+    
     elif player_choices == 'left':
         current_room -=1
         if current_room == -1:
             print("You can't go any further in that direction.")
-            current_room += 1
+            current_room = 0
+        elif location == 'monster' or location == 'boss_monster':
+            print("You cannot go past a monster. Therefore, you have been eaten and died.")
+            print("Game Over")
+            break
+    
     elif player_choices == 'up':
         if location == 'stairs_up':
-            current_floor += 1
-            if current_floor == 3:
-                print("You can not go any higher.")
-                current_floor -=1
+            current_floor -= 1
+            print("You went up the stairs.")
+            if current_floor == -1:
+                print("You cannot go any higher.")
+                current_floor = 0
         else:
-            print("You can not got up. There are no stairs leading up.")
+            print("There are no stairs here. You cannot go up.")
+    
+    elif player_choices == 'down':
+        if location == 'stairs_down':
+            current_floor += 1
+            print("You went down the stairs.")
+            if current_floor == 3:
+                print("You cannot go any lower.")
+                current_floor = 2
+        else:
+            print("There are no stairs here. You cannot go down.")
+
+    elif player_choices == 'grab':
+        if len(inventory) < 3:
+            if location == 'sword' or location == 'magic_stones':
+                inventory.append(location)
+                dungeon[current_floor][current_room] = 'empty'
+            elif location == 'prize':
+                print("You have grabbed the prize. You win!")
+                break
+            else:
+                print("There is nothing to be grabbed here.")
+        else:
+            print("You cannot put anything else in you inventory.")
+
+    elif player_choices == 'fight':
+        if location == 'monster':
+            if 'sword' in inventory:
+                print("You have slayed the monster, but your sword broke while fighting.")
+                inventory.remove('sword')
+                dungeon[current_floor][current_room] = 'empty'
+            else:
+                print("You do not have the necessary items for the battle. The monster has eaten you.")
+                print("Game Over.")
+                break
+        elif location == 'boss_monster':
+            if 'sword' in inventory and 'magic_stones' in inventory:
+                print("You have slayed the boss monster. ")
+                dungeon[current_floor][current_room] = 'empty'
+            else:
+                print("You do not have the necessary items for the battle. The monster has eaten you.")
+                print("Game Over")
+                break
+        else:
+            print("There is nothing to fight.")
+    
+    elif player_choices == 'inventory':
+        print("You have:")
+        print(' '.join(inventory))
+    
+    else:
+        print("That is not an option. Try again.")
+
+
+
+    
--- user_input_challenge.py
+++ user_input_challenge.py
@@ -11,3 +11,13 @@ many copies of the previous message. (Hint: order of operations exists in Python
 Print out that many copies of the previous message on separate lines. 
 (Hint: the string "\n" is the same as pressing the ENTER button)
 '''
+name = input("What is you name? ")
+age = int(input("How old are you? "))
+current_year = int(input("What is the current year? "))
+birth_year = current_year - age
+year_turn_100 = birth_year + 100
+print(f"You will turn 100 in {year_turn_100}.")
+repeat = int(input("Give me a random number. "))
+print(f"You will turn 100 in {year_turn_100}. \n" * repeat) 
+
+
